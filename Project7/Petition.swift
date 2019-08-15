42eec7aca89389dfdc1f9a77eb556c95dd0d41b7
elizeurs <13ders@gmail.com>
2019-08-15 00:29:23 -0300

    Parsing JSON using the Codable protocol

---
+++ Project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Elizeu RS on 15/08/19.
+//  Copyright © 2019 elizeurs. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+  var title: String
+  var body: String
+  var signatureCount: Int
+}
---
+++ Project7/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Elizeu RS on 15/08/19.
+//  Copyright © 2019 elizeurs. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+  var results: [Petition]
+}
